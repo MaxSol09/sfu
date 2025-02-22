80b438262a79a7edb6ff260a5248b8805edaaea8
Maxim <maksomsologor03@gmail.com>
2025-02-22 22:07:34 +0300

    commit 27

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ wss.on('connection', (ws, req) => {
 const app = express()
 
 const corsConfig = {
-    origin: ['https://sfu-counselor.vercel.app', 'http://localhost:3000', 'http://localhost:5173', 'http://192.168.1.190:3000'],
+    origin: ['https://sfu-counselor.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
