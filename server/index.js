993aeec90af1db70e48b281936ba96fe24dcc880
Maxim <maksomsologor03@gmail.com>
2025-03-02 20:14:33 +0300

    new update server

--- server/index.js
+++ server/index.js
@@ -58,8 +58,16 @@ wss.on('connection', (ws, req) => {
 
 const app = express()
 
+const corsConfig = {
+    origin: ['https://sfu-counselor.onrender.com', 'http://localhost:3000', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+
 app.use(express.json())
-app.use(cors())
+
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 
 
 app.use('/upload', express.static('images'))
