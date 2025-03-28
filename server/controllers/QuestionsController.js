66ac849b0de721f9a631549df422d8a9e12cc461
Maxim <maksomsologor03@gmail.com>
2025-03-28 16:09:30 +0300

    new update server

--- server/controllers/QuestionsController.js
+++ server/controllers/QuestionsController.js
@@ -274,7 +274,7 @@ export const createComment = async (req, res) => {
 
                 const message = `
                     <p>Текст: ${req.body.text}</p>
-                    <p>Перейдите по ссылке: <a href="${req.path}">Example Website</a></p>
+                    <p>Перейдите по ссылке: <a href="${req.get('host')}">Example Website</a></p>
                     <p>С уважением, Ваше приложение</p>
                 `;
 
