987c9811c82fd15b2efe743f9e045f3be142e4d9
Maxim <maksomsologor03@gmail.com>
2025-03-31 12:00:07 +0300

    new commit

--- server/controllers/QuestionsController.js
+++ server/controllers/QuestionsController.js
@@ -33,25 +33,6 @@ export const ModerationQuestion = async(req, res) => {
                 })
             })    
 
-            for (const el of users) {
-                if (el.role === 'Студент' && el.speciality && el.speciality.toLowerCase() === post.tags[0].toLowerCase()) {
-                    const message = `
-                        <p>Текст: новый вопрос</p>
-                        <p>Перейдите по ссылке: <a href="https://sfu-86v5.vercel.app/home/question/${post._id}">Ссылка на вопрос</a></p>
-                        <p>С уважением, ИКТИБ-СОВЕТНИК</p>
-                    `;
-
-                    if (el.email) {
-                        try {
-                            await sendMail("alekstook68@gmail.com", `новый вопрос по вашей тематике`, message); // Добавлена await и try/catch
-                            console.log(`Письмо отправлено студенту ${el.fullName}`);
-                        } catch (sendMailError) {
-                            console.error(`Ошибка отправки письма студенту ${el.fullName}:`, sendMailError);
-                        }
-                    }
-                }
-            }
-
             return res.json(post)
         }
         else{
