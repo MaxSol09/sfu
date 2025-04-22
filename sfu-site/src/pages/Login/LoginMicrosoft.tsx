5a3b35b5ef10f6e6397642087ec8ef3740932897
Maxim <maksomsologor03@gmail.com>
2025-04-22 23:14:26 +0300

    new commit

--- sfu-site/src/pages/Login/LoginMicrosoft.tsx
+++ sfu-site/src/pages/Login/LoginMicrosoft.tsx
@@ -1,91 +1,36 @@
 import { useIsAuthenticated, useMsal } from '@azure/msal-react';
 import React, { useEffect, useState } from 'react';
+import { msalInstance } from '../..';
 
 export const LoginMicrosoft: React.FC = () => {
-    const [isLogged, setIsLogged] = useState<boolean>(false);
-    const [userName, setUserName] = useState<string>('');
-    const [userEmail, setUserEmail] = useState<string>('');
 
-    const { instance } = useMsal();
-    const isAuthenticated = useIsAuthenticated();
-    const account = instance.getActiveAccount();
+  const [userEmail, setUserEmail] = useState<string | null>(null);
 
-    useEffect(() => {
-        if (isAuthenticated && account) {
-            setIsLogged(true);
-            setUserName(account.name || 'none name');
-            // Получаем email
-            getUserEmail(account.idToken || 'none id');
-        }
-    }, [isAuthenticated, account]);
-
-    const handleLogin = async () => {
-        let response = null;
-
-        try {
-            response = await instance.loginPopup({
-                scopes: ['User.Read'] // включите необходимый scope
-            });
-        } catch (err) {
-            console.log('Error during login >>> ', err);
-        }
-
-        if (response?.account) {
-
-            console.log(response.account)
-            instance.setActiveAccount(response.account);
-            setIsLogged(true);
-            setUserName(response.account.name || 'none name');
-            getUserEmail(response.account.idToken || 'none id');
-        } else {
-            setIsLogged(false);
-            alert('Failed to log in through Microsoft');
-        }
+  const login = () => {
+    const loginRequest = {
+      scopes: ["User.Read"]
     };
 
-    const getUserEmail = async (idToken: string) => {
-        const graphEndpoint = "https://graph.microsoft.com/v1.0/me";
-        const accessTokenResponse = await instance.acquireTokenSilent({
-            scopes: ["email"], // Включите необходимые привилегии
-            account: instance.getAllAccounts()[0],
-        });
+    msalInstance.loginPopup(loginRequest)
+      .then(response => {
+        const email = response.account.username;
+        console.log(response.account)
+        setUserEmail(email); // Сохраните Email пользователя
+        console.log("User email: ", email);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-        if (accessTokenResponse) {
-            const response = await fetch(graphEndpoint, {
-                headers: {
-                    Authorization: `Bearer ${accessTokenResponse.accessToken}`
-                }
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                setUserEmail(data.mail || 'none@mail.com'); // Установите email 
-
-                console.log('data >>>', data)
-            } else {
-                console.error('Error fetching user email:', response);
-            }
-        }
-    };
-
-    return (
-        <>
-            <div>LoginMicrosoft</div>
-                <div>
-                    <button onClick={handleLogin}>Entra Id </button>
-                </div>
-            {isLogged && (
-                <div>
-                    <p>User: {userName}</p>
-                    <p>Email: {userEmail}</p>
-                    <button>Token Info</button>
-                </div>
-            )}
-            {isLogged && (
-                <div>
-                    <button>Logout</button>
-                </div>
-            )}
-        </>
-    );
-};
+  return (
+    <div>
+      <h1>Microsoft Azure Authentication</h1>
+      {userEmail ? (
+        <p>Logged in as: {userEmail}</p>
+      ) : (
+        <button onClick={login}>Login with Microsoft</button>
+      )}
+    </div>
+  );
+}
