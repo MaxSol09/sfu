92bb491281c55eecb5bb129ec928eb976b22a173
Maxim <maksomsologor03@gmail.com>
2025-05-03 16:03:45 +0300

    new commit register

--- sfu-site/src/pages/Login/Login.tsx
+++ sfu-site/src/pages/Login/Login.tsx
@@ -56,7 +56,7 @@ import { resetStatusLogin } from '../../redux/auth'
      <div className='h-[100vh] flex w-full bg-cover' style={{backgroundImage: width < 700 ? `url(${Sfu2})` : ''}}>
        <img alt='sfu-logo' className='w-1/2 bg-img max-[700px]:hidden' src={Sfu}/>
        <div className='flex flex-col mt-[20vh] w-1/2 items-center max-[700px]:w-full'>
-        <div className='w-full flex flex-col justify-center items-center max-[700px]:shadow-custom-rounded max-[700px]:bg-white max-[700px]:w-[70%] max-[700px]:py-[30px] max-[400px]:w-[80%]'>
+        <div className='w-full rounded-[20px] flex flex-col justify-center items-center max-[700px]:shadow-custom-rounded max-[700px]:bg-white max-[700px]:w-[70%] max-[700px]:py-[30px] max-[400px]:w-[80%]'>
           <img className='w-[200px] max-[700px]:w-[150px]' src={Logo} alt="logo" />
           <h1 className='my-[20px] text-[30px] text-slate-500 max-[700px]:text-[25px]'>С возвращением!</h1>
           <div className='flex flex-col gap-[10px] w-full min-[1100px]:w-[80%] items-center'>
